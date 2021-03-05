73a57205f1a9c6e2cb769f85dd11fc202365e422
takahiroi <takahiroi.311@gmail.com>
2021-03-05 22:49:54 +0900

    edit for ci_test

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(StoreTableSeeder::class);
+        $this->call(UrlsTableSeeder::class);
+        $this->call(GeosTableSeeder::class);
     }
 }
