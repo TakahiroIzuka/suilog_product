2a9f17535301d10132d94b1a002d448df336c784
takahiroi <takahiroi.311@gmail.com>
2021-03-05 23:03:27 +0900

    fix dbseeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(StoreTableSeeder::class);
+        $this->call(StoresTableSeeder::class);
         $this->call(UrlsTableSeeder::class);
         $this->call(GeosTableSeeder::class);
     }
