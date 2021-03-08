6f91256b77b65998c7b7a0feeaa2b274ae2bade0
takahiroi <takahiroi.311@gmail.com>
2021-03-08 09:43:27 +0900

    rm :

--- public/js/distance_matrix_index.js
+++ public/js/distance_matrix_index.js
@@ -55,7 +55,7 @@ function initMap() {
                   document.getElementById("name" + i)
                     .textContent = stores[store_id].name;
                   document.getElementById("score" + i)
-                    .textContent = "スイログ評価: (" + stores[store_id].score + ")";
+                    .textContent = "スイログ評価 (" + stores[store_id].score + ")";
                   document.getElementById("type" + i)
                     .textContent = stores[store_id].type;
                   document.getElementById("station" + i)
