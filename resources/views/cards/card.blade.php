484da30434c99e1aad60e5d32572973305d5491e
takahiroi <takahiroi.311@gmail.com>
2021-03-18 11:47:42 +0900

    showページ img追加

--- resources/views/cards/card.blade.php
+++ resources/views/cards/card.blade.php
@@ -1,49 +1,64 @@
 <div class="card mb-3 mar-t-3">
-  <div class="card-body d-flex flex-row pb-2">
-    <i class="fas fa-beer fa-3x mr-1"></i>
-    <div>
-      <div class="d-flex">
-        <h3 class="font-weight-bold h4 card-title mb-1" id="{{ 'name' }}">
+  <div class="row m-3">
+    <div class="col-md-5 px-0">
+      <div class="mt-3">
+        <img class="d-block mx-auto img-fluid shadow-2-strong" src="{{ $store['store_pic'] }}" id="{{ 'store_pic' }}"></img>
+      </div>
+    </div>
+
+    <div class="col-md-7 px-2">
+      <div class="d-flex mt-3">
+        <h3 class="font-weight-bold h2 card-title mb-1" id="{{ 'name' }}">
           {{ $store['name'] }}
         </h3>
-        <a class="description px-3" id="{{ 'type' }}">
-          ジャンル : {{ $store['type'] }}
+      </div>
+
+      <div class="card-body pt-0 pb-2">
+        <div class="card-body pt-0 pb-2 pl-0">
+            <div class="card-text">
+              <card-like
+                :initial-is-liked-by='@json($store->isLikedBy(Auth::user()))'
+                :initial-count-likes='@json($store->count_likes)'
+                :authorized='@json(Auth::check())'
+                endpoint="{{ route('cards.like', ['store' => $store]) }}"
+              >
+              </card-like>
+            </div>
+          </div>
+        @if($store->smoking == '全席喫煙可')
+          <div class="description text-success" id="{{ 'smoking' }}">
+            {{ $store->smoking }}
+          </div>
+        @else 
+          <div class="description text-warning" id="{{ 'smoking' }}">
+            {{ $store->smoking }}
+          </div>
+        @endif
+        <a class="description" id="{{ 'type' }}">
+          ・ジャンル : {{ $store['type'] }}
         </a>
         <div class="font-weight-lighter" id="{{ 'score' }}">
-          スイログ評価 ( {{ $store['score'] }} )
+          ・食べログ評価 ( {{ $store['score'] }} )
+        </div>
+        <div class="description" id="{{ 'station' }}">
+          ・最寄駅 : {{ $store['station'] }}
+        </div>
+        <div class="row pl-3">
+          <div class="col-5 px-0">・ここから徒歩 : </div>
+          <div class="col-1 px-0 text-center orange-text" id="{{ 'time_h' }}"></div>
+          <div class="col-2 px-0" id="{{ 'hour' }}"></div>
+          <div class="col-1 px-0 text-center orange-text" id="{{ 'time_m' }}"></div>
+          <div class="col-1 px-0">分</div>
+        </div>
+        <div class="row pl-3">
+          <div class="col-3 px-0">・距離 : </div>
+          <div class="col-1 px-0 text-center green-text" id="{{ 'distance' }}"></div>
+          <div class="col-1 px-0">km</div>
         </div>
       </div>
     </div>
   </div>
-  <div class="card-body pt-0 pb-2">
-    <div class="description" id="{{ 'station' }}">
-      最寄駅 : {{ $store['station'] }}
-    </div>
-    <div class="d-flex">
-        <div class="description pr-3" id="{{ 'distance' }}"></div>
-        <div class="description" id="{{ 'time' }}"></div>
-    </div>
-    @if($store->smoking == '全席喫煙可')
-      <div class="description text-success" id="{{ 'smoking' }}">
-        {{ $store->smoking }}
-      </div>
-    @else 
-      <div class="description text-warning" id="{{ 'smoking' }}">
-        {{ $store->smoking }}
-      </div>
-    @endif
-  </div>
-  <div class="card-body pt-0 pb-2 pl-3">
-    <div class="card-text">
-      <card-like
-        :initial-is-liked-by='@json($store->isLikedBy(Auth::user()))'
-        :initial-count-likes='@json($store->count_likes)'
-        :authorized='@json(Auth::check())'
-        endpoint="{{ route('cards.like', ['store' => $store]) }}"
-      >
-      </card-like>
-    </div>
-  </div>
+
 
   <div class="card-body mt10">
     <div class="row">
